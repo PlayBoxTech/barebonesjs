19df4a504f6bec27627746819ced0d0e4aebf7bb
Chris Moody <design@playbox.tech>
2023-07-27 22:59:57 -0500

    strobe light is on!

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-window.onload = () =>  {
+window.onload = () => {
     var objDate = new Date();
     var hours = objDate.getHours();
-    const hoursText = document.getElementById('hours');
+    const mousePosText = document.getElementById('mouse-pos');
+    let mousePos = { x: undefined, y: undefined };
+    const crazyElement = document.getElementById("crazy");
+    const h1 = document.getElementsByTagName("h1");
 
-    if (hours >= 20) {
+    const hoursText = document.getElementById('hours');
+    
+
+  /*  if (hours >= 20) {
         document.body.style.backgroundColor = "#663399";
     }
 
@@ -13,8 +19,20 @@ window.onload = () =>  {
 
     if (hours <= 8) {
         document.body.style.backgroundColor = "#DDA0DD";
-    }
+    }*/
 
     console.log(hours);
     hoursText.textContent = toString(hours);
+    window.addEventListener('mousemove', (event) => {
+        mousePos = { x: event.clientX, y: event.clientY };
+        mousePosText.textContent = `(${mousePos.x}, ${mousePos.y})`;
+          // Calculate color values based on mouse position
+      const red = mousePos.x % 256;
+      const green = mousePos.y % 256;
+      const blue = (mousePos.x + mousePos.y) % 256;
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+     h1[0].style.color = `rgb(${green}, ${red}, ${blue})`;
+    });
 }
+
+
